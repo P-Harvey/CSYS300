6c9c9e9169c8aa1c0ade0d5f0db15312d6526295
P-Harvey <62023010+P-Harvey@users.noreply.github.com>
2021-11-18 14:27:10 -0500

    Add files via upload

--- Project/CSYS_300_PLHARVEY_PROJECT.m
+++ Project/CSYS_300_PLHARVEY_PROJECT.m
@@ -2,20 +2,22 @@
 %% Create an allotaxonomograph %%
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
-name_1 = 'Direct Farm Sales 2007';
-name_2 = 'Direct Farm Sales 2012';
-name_3 = 'DFS-07-12-rank-div-alpha-5';
+name_1 = 'Vegetable Acres PTH 2007';
+name_2 = 'Vegetable Acres PTH 2012';
+name_3 = 'VEGPTH-07-12-rank-div-alpha-1-12';
 daterange = '2007 - 2012';
 
 %% Load Data Manually from .txt unless you're smart with MATLAB...
 
-var1(1).table = DIRSALESFARMS07;
-var1(2).table = DIRSALESFARMS12;
+var(1).table = VEGACRESPTH07;
+var(2).table = VEGACRESPTH12;
 
 indices = [1 2];
 for i=1:2
-    elements(i).types = var1(indices(i)).table{:,1};
-    elements(i).counts = var1(indices(i)).table{:,2};
+    elements(i).types = var(indices(i)).table{:,1};
+    elements(i).counts = var(indices(i)).table{:,2};
+    %% How to calculate prob from count...
+    %elements(i).probs = 
 
     elements(i).ranks = tiedrank(elements(i).counts);
     elements(i).totalunique = length(elements(i).types);
@@ -47,15 +49,17 @@ settings.max_shift_string_length = 25;
 settings.imageformat.open = 'no';
 
 settings.plotkind = 'rank';
+%settings.plotkind = 'probability';
 
 settings.instrument = 'rank divergence';
+%settings.instrument = 'probability divergence';
 
 %% move the shift (adds to 0.60)
 settings.xoffset = +0.00;
 
 %% alphavals = [(0:18)/12, 2, 3, 5, 10, Inf]';
 
-settings.alpha = 5;
+settings.alpha = 1/12;
 
 tag = sprintf(name_3);
 
